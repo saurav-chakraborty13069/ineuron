3454809f972fca6597c1dcc515b7dd235ab67fda
saurav.chakraborty13069 <saurav.chakraborty13069@gmail.com>
2020-06-12 18:16:49 +0530

    added changed files

--- projects/imdb_scraping/scrapper.py
+++ projects/imdb_scraping/scrapper.py
@@ -6,23 +6,17 @@ from random import randint
 from time import time
 from logger import App_Logger
 from warnings import warn
+import logger
+from datetime import  datetime
 
 
-def scrape():
-    pages = [str(i) for i in range(1,2)]
-    years_url = [str(i) for i in range(2017,2018)]
-    names = []
-    years = []
-    ratings = []
-    metascores = []
-    votes = []
-    descriptions = []
-    certficates = []
-    runtimes = []
-    genres = []
-    directors = []
-    stars = []
-    gross_earnings = []
+def scrape(y, log_writer, file_object):
+    #log_writer = logger.App_Logger()
+    #file_object = open("logs/imdb_scraper_test-{}.txt".format(datetime.now().date()), 'a+')
+    pages = [str(i) for i in range(1,100, 50)]
+    #years_url = [str(i) for i in range(2017,2018)]
+
+    movies = []
 
     headers = {"Accept-Language": "en-US, en;q=0.5"}
     start_time = time()
@@ -34,119 +28,97 @@ def scrape():
     # html_soup = BeautifulSoup(response.text, 'html.parser')
     # movie_containers = html_soup.find_all('div', class_ = 'lister-item mode-advanced')
 
-    for year_url in years_url:
-        for page in pages:
-            response = requests.get('http://www.imdb.com/search/title?release_date=' + year_url +
-            '&sort=num_votes,desc&page=' + page, headers = headers)
 
-            sleep(randint(8, 15))
+    for page in pages:
+        #url = 'http://www.imdb.com/search/title?release_date={}&sort=num_votes,desc&page={}'.format(y, page)
+        url = 'https://www.imdb.com/search/title/?release_date={}-01-01,{}-12-31&sort=num_votes,desc&start={}&ref_=adv_nxt'.format(y,y, page)
+        log_writer.log(file_object, 'reading from the url {}'.format(url))
+        response = requests.get(url, headers = headers)
+        log_writer.log(file_object, 'received the response')
+        sleep(randint(8, 15))
 
-            request += 1
-            elapsed_time = time() - start_time
-            print('Request:{}; Frequency: {} requests/s'.format(request, request / elapsed_time))
-            # if response.status_code != 200:
-            #     warn('Request: {}; Status code: {}'.format(request, response.status_code))
-            #
-            #     # Break the loop if the number of requests is greater than expected
-            # if request > 72:
-            #     warn('Number of requests was greater than expected.')
-            #     break
+        request += 1
+        elapsed_time = time() - start_time
+        log_writer.log(file_object, 'Request:{}; Frequency: {} requests/s'.format(request, request / elapsed_time))
+        page_html = BeautifulSoup(response.text, 'html.parser')
+        mv_containers = page_html.find_all('div', class_='lister-item mode-advanced')
+        #print(mv_containers[0])
+        log_writer.log(file_object, 'Start of getting tag details for page {}'.format(page))
+        for container in mv_containers:
+        #if container.find('div', class_ = 'ratings-metascore') is not None:
+            try:
+                name = container.h3.a.text
+            except:
+                name = 'No Name'
 
-            page_html = BeautifulSoup(response.text, 'html.parser')
+            try:
+                year = container.h3.find('span',class_ = 'lister-item-year text-muted unbold').text
+            except:
+                year = 'No year'
 
-            mv_containers = page_html.find_all('div', class_='lister-item mode-advanced')
+            try:
+                rating = container.strong.text
+            except:
+                rating = 'No Rating'
 
-            for container in mv_containers:
-            #if container.find('div', class_ = 'ratings-metascore') is not None:
-                try:
-                    name = container.h3.a.text
-                    names.append(name)
-                except:
-                    name = 'No Name'
-                    names.append(name)
+            try:
+                metascore = container.find('span', class_ = 'metascore favorable').text
+            except:
+                metascore = "No metascore"
 
-                try:
-                    year = container.h3.find('span',class_ = 'lister-item-year text-muted unbold').text
-                    years.append(year)
-                except:
+            try:
+                vote = container.find('span', attrs = {'name': "nv"}).text
+            except:
+                vote = "no vote"
 
-                    year = 'No year'
-                    years.append(year)
+            try:
+                desc = container.find_all('p', class_='text-muted')[1].text
+            except:
+                desc = 'No Description'
 
-                try:
-                    rating = container.strong.text
-                    ratings.append(rating)
-                except:
-                    rating = 'No Rating'
-                    ratings.append(rating)
+            try:
+                cert = container.find('span', class_='certificate').text
+            except:
+                cert = 'No cert'
 
-                try:
-                    metascore = container.find('span', class_ = 'metascore favorable').text
-                    metascores.append(metascore)
-                except:
-                    metascore = "No metascore"
-                    metascores.append(metascore)
+            try:
+                runtime = container.find('span', class_='runtime').text
+            except:
+                runtime = 'No runtime'
 
-                try:
-                    vote = container.find('span', attrs = {'name': "nv"}).text
-                    votes.append(vote)
-                except:
-                    vote = "no vote"
-                    votes.append(vote)
+            try:
+                genre = container.find('span', class_='genre').text
+            except:
+                genre = 'No genre'
 
-                try:
-                    desc = container.find_all('p', class_='text-muted')[1].text
-                    descriptions.append(desc)
-                except:
-                    desc = 'No Description'
-                    descriptions.append(desc)
+            try:
+                director = container.find_all('p')[2].a.text
+            except:
+                director = 'No director'
 
-                try:
-                    cert = container.find('span', class_='certificate').text
-                    certficates.append(cert)
-                except:
-                    cert = 'No cert'
-                    certficates.append(cert)
+            try:
+                st = container.find_all('p')[2].find_all('a')[1:]
+                star = [s.text for s in st]
+            except:
+                star = 'No Star'
 
-                try:
-                    runtime = container.find('span', class_='runtime').text
-                    runtimes.append(runtime)
-                except:
-                    runtime = 'No runtime'
-                    runtimes.append(runtime)
+            try:
+                gross = container.find_all('span', attrs = {'name': "nv"})[1].text
+            except:
+                gross = 'No Gross'
 
-                try:
-                    genre = container.find('span', class_='genre').text
-                    genres.append(genre)
-                except:
-                    genre = 'No genre'
-                    genres.append(genre)
+            mydict = {'Name': name, 'Year': year, 'Rating': rating, 'Metascore': metascore,
+                      'Vote': vote, 'Description': desc, 'Certificate': cert, 'Runtime': runtime,
+                      'Genre': genre, 'Director': director,'Stars': star, 'Earning': gross}
+            movies.append(mydict)
 
-                try:
-                    director = container.find_all('p')[2].a.text
-                    directors.append(director)
-                except:
-                    director = 'No director'
-                    directors.append(director)
+        log_writer.log(file_object, 'End of getting tag details for page {}'.format(page))
 
-                try:
-                    st = container.find_all('p')[2].find_all('a')[1:]
-                    star = [s.text for s in st]
-                    stars.append(star)
-                except:
-                    stars.append('No Star')
-
-                try:
-                    gross = container.find_all('span', attrs = {'name': "nv"})[1].text
-                    gross_earnings.append(gross)
-                except:
-                    gross_earnings.append('No Earning')
+    log_writer.log(file_object, 'returning movies')
+    return movies
 
 
-    mydict = {'name': names, 'year': years, 'rating':ratings, 'metascore': metascores, 'vote': votes, 'description':descriptions,
-              'certificate':certficates, 'runtime':runtimes, 'genre': genres, 'director':directors, 'stars': stars, 'earning':gross_earnings}
-    df = pd.DataFrame(mydict)
-    df.to_csv('first50movies.csv')
-    #print(df.info())
 
-    return df
+
+
+
